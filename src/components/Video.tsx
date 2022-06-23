45776d654c869474813a51ee1fb1f3e2ddd58a1e
Leonardo Teixeira <leonardo.lp2@gmail.com>
2022-06-22 22:43:22 -0300

    Stilyng video component

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,11 @@
 export function Video() {
     return (
         <div className="flex-1">
-            <h1>Video</h1>
+            <div className="bg-black flex justify-center">
+                <div className="h-full w-full max-w-[1100px] max-h-[68vh] aspect-video">
+
+                </div>
+            </div>
         </div>
     )
 }
